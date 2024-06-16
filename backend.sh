3d41241f4e6fa89e88e1eeea630682e4b7d4a6b1
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-06-16 14:12:03 -0500

    adding step to removed old, content in backend

--- backend.sh
+++ backend.sh
@@ -3,6 +3,7 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd expense
 cp backend.service /etc/systemd/system/backend.service
+rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
@@ -16,4 +17,3 @@ systemctl start backend
 dnf install mysql -y
 mysql -h 172.31.39.174 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
-#
