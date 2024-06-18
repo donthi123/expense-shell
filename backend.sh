ae8ab5d140ab21cd5d34553a94adb45fc7ecebc4
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-06-18 18:51:20 -0500

    adding expense reverse proxy file and steps script

--- backend.sh
+++ backend.sh
@@ -7,7 +7,7 @@ rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
-unzip /tmp/backend.zipcd /app
+unzip /tmp/backend.zip
 cd /app
 npm install
 
@@ -15,4 +15,4 @@ systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
 dnf install mysql -y
-mysql -h 172.31.39.174 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.35.28 -uroot -pExpenseApp@1 < /app/schema/backend.sql
