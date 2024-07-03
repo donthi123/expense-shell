02fcff39a9cdcec3850e8c4f38ad400e41f862ef
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-03 17:41:23 -0500

    giving status of every operation

--- backend.sh
+++ backend.sh
@@ -9,54 +9,54 @@ fi
 
 print_Task_Heading "Disable default NodeJS version module"
 dnf module disable nodejs -y &>>/tmp/expense.log
-echo $?
+check_status $?
 
 print_Task_Heading "enable NodeJS module for v20"
 dnf module enable nodejs:20 -y &>>/tmp/expense.log
-echo $?
+check_status $?
 
 print_Task_Heading "install NodeJS"
 dnf install nodejs -y &>>/tmp/expense.log
-echo $?
+check_status $?
 
 print_Task_Heading "Adding Application User"
 useradd expense &>>/tmp/expense.log
-echo $?
+check_status $?
 
 print_Task_Heading "Copy Backend Service file"
 
 cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
-echo $?
+check_status $?
 
 print_Task_Heading "Clean the old Content"
 rm -rf /app &>>/tmp/expense.log
-echo $?
+check_status $?
 
 print_Task_Heading "Create App Directory"
 mkdir /app &>>/tmp/expense.log
-echo $?
+check_status $?
 
 print_Task_Heading "Download App Content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
-echo $?
+check_status $?
 
 print_Task_Heading "Extract App Content"
 cd /app &>>/tmp/expense.log
 unzip /tmp/backend.zip &>>/tmp/expense.log
-echo $?
+check_status $?
 
 print_Task_Heading "Download NodeJS Dependencies"
 cd /app &>>/tmp/expense.log
 npm install &>>/tmp/expense.log
-echo $?
+check_status $?
 print_Task_Heading "start Backend Service"
 systemctl daemon-reload &>>/tmp/expense.log
 systemctl enable backend &>>/tmp/expense.log
 systemctl start backend &>>/tmp/expense.log
-echo $?
+check_status $?
 print_Task_Heading "Install MySQL Client"
 dnf install mysql -y &>>/tmp/expense.log
-echo $?
+check_status $?
 print_Task_Heading "Load Schema"
 mysql -h 172.31.82.154 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>/tmp/expense.log
-echo $?
+check_status $?
--- common.sh
+++ common.sh
@@ -2,3 +2,10 @@ print_Task_Heading() {
   echo $1
   echo "############ $1 ######" &>>/tmp/expense.log
 }
+check_status() {
+  if [ $1 -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
+}
