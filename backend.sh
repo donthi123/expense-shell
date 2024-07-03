2ca5ae70be324b79816d43b29264f21c5856b919
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-03 17:10:57 -0500

    handling password missing in backend

--- backend.sh
+++ backend.sh
@@ -1,6 +1,11 @@
 source common.sh
 mysql_root_password=$1
+# If password is not provided then we will exit
+if [ -z "${mysql_root_password}"]; then
+  echo Input Password is missing
+  exit 1
 
+fi
 
 print_Task_Heading "Disable default NodeJS version module"
 dnf module disable nodejs -y &>>/tmp/expense.log
