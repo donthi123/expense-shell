5abcf1f247e2134a4baf5d29003e15ec75d041bd
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-03 18:56:12 -0500

    handling mysql password setup

--- mysql.sh
+++ mysql.sh
@@ -3,6 +3,7 @@ if [ -z "${mysql_root_password}" ]; then
     echo input password missing
     exit 1
 fi
+
 print_Task_Heading "install MySQL server"
 dnf install mysql-server -y &>>$LOG
 Check_Status $?
@@ -17,5 +18,4 @@ echo 'show databases' |mysql -h 172.31.82.154 -uroot -p${mysql_root_password} &>
 if [ $? -ne 0 ]; then
     mysql_secure_installation --set-root-pass {mysql_root_password} &>>$LOG
 fi
-
 Check_Status $?
