056493e5e049ea91901e2aa9366b248bd0082071
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-03 18:27:18 -0500

    update heading for mySql

--- mysql.sh
+++ mysql.sh
@@ -3,15 +3,15 @@ if [ -z "${mysql_root_password}" ]; then
     echo input password missing
     exit 1
 fi
-print_Task_Heading "copy Nginx configuration"
+print_Task_Heading "install MySQL server"
 dnf install mysql-server -y &>>$LOG
 Check_Status $?
 
-print_Task_Heading "copy Nginx configuration"
+print_Task_Heading "start MySQL service"
 systemctl enable mysqld &>>$LOG
 systemctl start mysqld &>>$LOG
 Check_Status $?
 
-print_Task_Heading "copy Nginx configuration"
+print_Task_Heading "setup MySQL password"
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG
 Check_Status $?
