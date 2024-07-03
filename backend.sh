4c3e6a2e2473194dd9845e208326db0ea3909382
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-03 15:01:03 -0500

    moving the heading printing to a function also adding the same message to log output as well

--- backend.sh
+++ backend.sh
@@ -1,36 +1,47 @@
 mysql_root_password=$1
+
 print_Task_Heading() {
   echo $1
   echo "############ $1 ######" &>>/tmp/expense.log
+}
 print_Task_Heading "Disable default NodeJS version module"
 dnf module disable nodejs -y &>>/tmp/expense.log
 echo $?
+
 print_Task_Heading "enable NodeJS module for v20"
 dnf module enable nodejs:20 -y &>>/tmp/expense.log
 echo $?
+
 print_Task_Heading "install NodeJS"
 dnf install nodejs -y &>>/tmp/expense.log
 echo $?
+
 print_Task_Heading "Adding Application User"
 useradd expense &>>/tmp/expense.log
 echo $?
+
 print_Task_Heading "Copy Backend Service file"
 
 cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
 echo $?
+
 print_Task_Heading "Clean the old Content"
 rm -rf /app &>>/tmp/expense.log
 echo $?
+
 print_Task_Heading "Create App Directory"
 mkdir /app &>>/tmp/expense.log
 echo $?
+
 print_Task_Heading "Download App Content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
 echo $?
+
 print_Task_Heading "Extract App Content"
 cd /app &>>/tmp/expense.log
 unzip /tmp/backend.zip &>>/tmp/expense.log
 echo $?
+
 print_Task_Heading "Download NodeJS Dependencies"
 cd /app &>>/tmp/expense.log
 npm install &>>/tmp/expense.log
